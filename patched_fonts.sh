c8beea52975df94cff2364a023e984e33c480e4e
William Chong <william-chong@outlook.com>
2023-11-25 08:43:41 +0400

    Prefer dark menus and resize tmux

--- patched_fonts.sh
+++ patched_fonts.sh
@@ -4,9 +4,9 @@ mkdir -p ~/.local/share/fonts
 
 if [ -d ~/.local/share/fonts ]
 then
-  RegularFont=https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf?raw=true
-  BoldFont=https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Hack/Bold/complete/Hack%20Bold%20Nerd%20Font%20Complete.ttf?raw=true
-  ItalicFont=https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Hack/Italic/complete/Hack%20Italic%20Nerd%20Font%20Complete.ttf?raw=true
+  RegularFont=https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/HackNerdFont-Regular.ttf?raw=true
+  BoldFont=https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Bold/HackNerdFont-Bold.ttf?raw=true
+  ItalicFont=https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Italic/HackNerdFont-Italic.ttf?raw=true
 
   curl -fLo ~/.local/share/fonts/'Hack Regular Nerd Font Complete.ttf' $RegularFont
   curl -fLo ~/.local/share/fonts/'Hack Bold Nerd Font Complete.ttf' $BoldFont
