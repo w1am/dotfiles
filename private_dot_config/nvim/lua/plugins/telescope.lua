444758c8f55c9ab7475aefa91532e5761458bba1
w1am <33353798+w1am@users.noreply.github.com>
2026-05-31 21:31:04 +0400

    Show dotfiles in Telescope find_files, ignore build dirs

--- private_dot_config/nvim/lua/plugins/telescope.lua
+++ private_dot_config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,6 @@
 return {
-    'nvim-telescope/telescope.nvim', version = '*',
+    'nvim-telescope/telescope.nvim',
+    version = '*',
     dependencies = {
         'nvim-lua/plenary.nvim',
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
@@ -9,9 +10,25 @@ return {
         require('telescope').setup({})
 
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = 'Telescope find files' })
+
+        vim.keymap.set('n', '<leader>sf', function()
+            builtin.find_files({
+                hidden = true,
+                file_ignore_patterns = {
+                    '%.git/',
+                    'node_modules/',
+                    '__pycache__/',
+                    '%.venv/',
+                    'obj/',
+                    'target/',
+                    '%.gradle/',
+                    '%.DS_Store',
+                },
+            })
+        end, { desc = 'Telescope find files' })
+
         vim.keymap.set('n', '<leader>gs', builtin.live_grep, { desc = 'Telescope live grep' })
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-    end
+    end,
 }
